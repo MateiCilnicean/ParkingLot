fbd69abfc9d02e4fb01f417e3af31f59b1017e3c
MateiCilnicean <cilniceanro@gmail.com>
2024-11-18 10:33:34 +0200

    Part5: Created the entities

--- src/main/java/com/parking/parkinglot/Cars.java
+++ src/main/java/com/parking/parkinglot/Cars.java
@@ -1,4 +1,5 @@
-package com.parking.parkinglot;import jakarta.servlet.*;
+package com.parking.parkinglot;
+import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.IOException;
---
+++ src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,37 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private Long Id;
+    private String licensePlate;
+    private String parkingSpot;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+}
